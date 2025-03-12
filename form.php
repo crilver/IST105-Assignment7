c42b0173b413d6e5bcf11704422f8e951978729e
crilverst <cristobal.lara@smarttwigs.com>
2025-03-11 18:05:24 -0700

    Fix: Add a new input for user threshold

---
+++ form.php
@@ -0,0 +1,11 @@
+<html>
+    <form action="process.php" method="get">
+        <label>Input a list of integers separated by commas:</label>
+        <input type="text" name="a" required>
+        <br>
+        <label>Insert an integer threshold:</label>
+        <input type="number" name="b" required>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</html>
